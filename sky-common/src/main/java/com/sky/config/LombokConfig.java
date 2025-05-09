22050ceb4dfee7098ddea4db8bbc349b7bad32e3
Richal <lirichal3@gmail.com>
2025-05-09 17:33:30 +0800

    IDEA中启动项目时出现的NoSuchFieldError: Class com.sun.tools.javac.tree.JCTree$JCImport does not have member field 'com.sun.tools.javac.tree.JCTree qualid'错误

---
+++ sky-common/src/main/java/com/sky/config/LombokConfig.java
@@ -0,0 +1,12 @@
+package com.sky.config;
+
+/**
+ * 解决IDEA和Lombok不兼容问题的配置类
+ * 这个类的存在可以帮助IDE正确处理Lombok注解
+ */
+public class LombokConfig {
+    /**
+     * 此配置类不需要任何实际内容
+     * 仅作为标记，帮助IDE理解项目使用的Lombok版本
+     */
+} 
